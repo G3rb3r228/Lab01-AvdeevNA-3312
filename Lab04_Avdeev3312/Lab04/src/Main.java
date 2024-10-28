f8dc569a7c29a770a1c6c96645eed752f1364917
G3rb3r228 <avdeevcool12@gmail.com>
2024-10-28 21:47:23 +0300

    Add files via upload

---
+++ Lab04_Avdeev3312/Lab04/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Laboratory work №4
+ *
+ * @author  Avdeev Nikita
+ * @version  1.2
+ */
+public class Main {
+    /**
+     * @param args - the line being entered (the startup parameter);
+     */
+    public static void main(String[] args) {
+        App.CoffeeShop();
+    }
+}
